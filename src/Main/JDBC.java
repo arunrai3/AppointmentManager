534bdeac5a4c27575452488723448d7adbc9d6b6
Arun Rai <arunrai202@gmail.com>
2023-11-22 12:42:50 -0500

    edit the connection to SQL database

--- src/Main/JDBC.java
+++ src/Main/JDBC.java
@@ -16,8 +16,8 @@ public abstract class JDBC {
     private static final String databaseName = "client_schedule";
     private static final String jdbcUrl = protocol + vendor + location + databaseName + "?connectionTimeZone = LOCAL"; // LOCAL
     private static final String driver = "com.mysql.cj.jdbc.Driver"; // Driver reference
-    private static final String userName = "root"; // Username
-    private static String password = "tigers123"; // Password
+    private static final String userName = "sqlUser"; // Username
+    private static String password = "Passw0rd!"; // Password
     public static Connection connection;  // Connection Interface
     
     
